02f156b7385823bbc03203b666be54044755be7c
MateoMorvan <mateo.morvan.2205@gmail.com>
2025-09-06 18:04:25 +0200

    Initial Commit

---
+++ Mateo aime Lou-Anne/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mateo aime Lou-Anne
+//
+//  Created by Mateo Morvan on 06/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
